96608fac972989db88dd42c2d844b81db3dc1c7e
BlackImpostor <106878493+SkyBuilder1717@users.noreply.github.com>
2023-04-07 19:19:07 +0300

    C# file script

---
+++ HelloTeacher.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloTeacher : MonoBehaviour
+{
+    void Start()
+    {
+        // Привет Лев Родиков :)
+        print("Hello, Teacher!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
